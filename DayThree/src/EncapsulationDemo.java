ee668ac59a9b168b28d5b70a5c5b23b99de3c05c
mohit <mohit@DESKTOP-017IEIJ>
2023-04-15 22:13:46 +0530

    First Commit

--- DayThree/src/EncapsulationDemo.java
+++ DayThree/src/EncapsulationDemo.java
@@ -1,9 +1,12 @@
 
-public class EncapsulationDemo {
-
+class EncapsulationDemo{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		EncapStudent s=new EncapStudent();  
 
+		s.setName("Mohith Reddy");  
+
+		System.out.println(s.getName());  
 	}
 
 }
