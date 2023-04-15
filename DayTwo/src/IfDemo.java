9a14b49becb96ba416cabd5385e76e27564d1949
mohit <mohit@DESKTOP-017IEIJ>
2023-04-15 16:59:43 +0530

    First Commit

--- DayTwo/src/IfDemo.java
+++ DayTwo/src/IfDemo.java
@@ -3,7 +3,12 @@ public class IfDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int i = 10;
+		 
+        if (i < 15)
+            System.out.println("10 is less than 15");
+ 
+        System.out.println("Outside if-block");
 	}
 
 }
