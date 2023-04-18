b8304c0b185b0c6d0e767a55616d7051bae631b4
mohit <mohit@DESKTOP-017IEIJ>
2023-04-18 22:46:35 +0530

    First Commit

--- DayFour/src/com/cg/mainclass/Main.java
+++ DayFour/src/com/cg/mainclass/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyClass mc = new MyClass();
-		com.cg.generaljavaclass.
+		mc.method1();
 	}
 
 }
