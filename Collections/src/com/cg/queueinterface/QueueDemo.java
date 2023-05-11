8f408fa66c1a4e64de8e2baa2af171fac36279f7
mohit <mohit@DESKTOP-017IEIJ>
2023-05-11 18:31:20 +0530

    First Commit

---
+++ Collections/src/com/cg/queueinterface/QueueDemo.java
@@ -0,0 +1,13 @@
+package com.cg.queueinterface;
+
+import java.util.Queue;
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
