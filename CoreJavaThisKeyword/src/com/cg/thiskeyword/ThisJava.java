356aa5f1329bae9a41cd3f7c9b9f157c78ad1b39
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2023-05-16 12:01:11 +0530

    First Commit

--- CoreJavaThisKeyword/src/com/cg/thiskeyword/ThisJava.java
+++ CoreJavaThisKeyword/src/com/cg/thiskeyword/ThisJava.java
@@ -31,10 +31,10 @@ class Test2 extends Test1{
 		this.b = b;
 	}
 	void display() {
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(a);
-		System.out.println(b);
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
 	}
 }
 
