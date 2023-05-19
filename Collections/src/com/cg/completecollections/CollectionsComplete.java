5a5d988565922da90516ddebab933964026f99d3
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2023-05-19 17:38:53 +0530

    First Commit

--- Collections/src/com/cg/completecollections/CollectionsComplete.java
+++ Collections/src/com/cg/completecollections/CollectionsComplete.java
@@ -34,49 +34,6 @@ public class CollectionsComplete {
         treeSet.add(4);
         System.out.println("TreeSet: " + treeSet);
 
-        // HashMap example
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        hashMap.put("Apple", 10);
-        hashMap.put("Banana", 20);
-        hashMap.put("Orange", 15);
-        System.out.println("HashMap: " + hashMap);
-
-        // TreeMap example
-        TreeMap<String, Integer> treeMap = new TreeMap<>();
-        treeMap.put("Apple", 10);
-        treeMap.put("Banana", 20);
-        treeMap.put("Orange", 15);
-        System.out.println("TreeMap: " + treeMap);
-
-        // PriorityQueue example
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(5);
-        priorityQueue.add(2);
-        priorityQueue.add(8);
-        priorityQueue.add(1);
-        priorityQueue.add(4);
-        System.out.println("PriorityQueue: " + priorityQueue);
-
-        // ArrayDeque example
-        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
-        arrayDeque.add("Alice");
-        arrayDeque.add("Bob");
-        arrayDeque.add("Charlie");
-        System.out.println("ArrayDeque: " + arrayDeque);
-
-        // Stack example
-        Stack<String> stack = new Stack<>();
-        stack.push("Alice");
-        stack.push("Bob");
-        stack.push("Charlie");
-        System.out.println("Stack: " + stack);
-
-        // Vector example
-        Vector<Integer> vector = new Vector<>();
-        vector.add(10);
-        vector.add(20);
-        vector.add(30);
-        System.out.println("Vector: " + vector);
     }
 }
 
