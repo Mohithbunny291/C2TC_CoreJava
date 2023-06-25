6aea9113b4062c104683260c2b879c164a163b53
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2023-06-25 17:48:11 +0530

    First Commit

--- DaySix/src/com/cg/hieriachialinheritance/HIDemo.java
+++ DaySix/src/com/cg/hieriachialinheritance/HIDemo.java
@@ -21,7 +21,7 @@ class Cat extends Animal{
 public class HIDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Dog d = new Dog();
 		d.bark();
 		d.eat();
